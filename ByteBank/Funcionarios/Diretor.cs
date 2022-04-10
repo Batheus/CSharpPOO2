ef79cc75e0b5015271f34847715c0120b3278d2a
Matheus <batheus.dev@gmail.com>
2022-04-10 15:08:18 -0300

    Parte 2

--- ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/Funcionarios/Diretor.cs
@@ -6,15 +6,15 @@ using System.Threading.Tasks;
 
 namespace ByteBank.Funcionarios
 {
-    public class Diretor
+    // Os : foi utilizado para que o Diretor herde as características e comportamento do Funcionario
+    // Por isso não precisamos repetir as declarações de Nome, CPF e Salário
+    public class Diretor : Funcionario
     {
-        public string Nome { get; set; }
-        public string CPF { get; set; }
-        public double Salario { get; set; }
-
-        public double GetBonificacao()
+        // Foi utilizado override para que ele sobrescreva o GetBonificacao() do Funcionario
+        public override double GetBonificacao()
         {
-            return Salario;
+            // base serve pra gente puxar o GetBonificacao da classe base (Funcionario)
+            return Salario + base.GetBonificacao();
         }
     }
 }
