65f322bd84bb4b108da356d3d41d9033f157ecb3
Matheus <batheus.dev@gmail.com>
2022-04-13 14:38:30 -0300

    Parte 3

--- ByteBank/Funcionarios/Diretor.cs
+++ ByteBank/Funcionarios/Diretor.cs
@@ -10,11 +10,22 @@ namespace ByteBank.Funcionarios
     // Por isso não precisamos repetir as declarações de Nome, CPF e Salário
     public class Diretor : Funcionario
     {
+        public Diretor(string cpf) : base(5000, cpf) //Foi estipulado o salário de 5000 para todos os diretos como regra de negócio
+        {
+            Console.WriteLine("Criando DIRETOR");
+        }
+
         // Foi utilizado override para que ele sobrescreva o GetBonificacao() do Funcionario
         public override double GetBonificacao()
         {
             // base serve pra gente puxar o GetBonificacao da classe base (Funcionario)
-            return Salario + base.GetBonificacao();
+            //return Salario + base.GetBonificacao();
+            return Salario * 0.5;
+        }
+
+        public override void AumentarSalario()
+        {
+            Salario *= 1.15;
         }
     }
 }
--- ByteBank/Program.cs
+++ ByteBank/Program.cs
@@ -11,50 +11,31 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
-
-            Funcionario carlos = new Funcionario();
-
-            carlos.Nome = "Carlos";
-            carlos.CPF = "123.456.789-10";
-            carlos.Salario = 2000;
-            gerenciador.Registrar(carlos);
-
-            /* Por termos herdado as características do Funcionário na classe Diretor, 
-             * podemos criar um Diretor até mesmo dessa forma:
-             * Funcionario pedro = new Diretor();
-            */
-
-            Diretor rodolfo = new Diretor();
-
-            rodolfo.Nome = "Rodolfo";
-            rodolfo.CPF = "123.654.789-10";
-            rodolfo.Salario = 8000;
-            gerenciador.Registrar(rodolfo);
-
-
-            Console.WriteLine("Nome do funcionário: " + carlos.Nome);
-            Console.WriteLine("CPF do funcionário: " + carlos.CPF);
-            Console.WriteLine("Salário do funcionário: " + carlos.Salario);
-
-            Console.WriteLine("A bonificação do funcionário " + carlos.Nome + " será de: R$" + carlos.GetBonificacao());
-            Console.WriteLine("O salário total do funcionário " + carlos.Nome + " será de: R$" + (carlos.GetBonificacao() + carlos.Salario));
-
-
-            Console.WriteLine( );
-
-            Console.WriteLine("Nome do funcionário: " + rodolfo.Nome);
-            Console.WriteLine("CPF do funcionário: " + rodolfo.CPF);
-            Console.WriteLine("Salário do funcionário: " + rodolfo.Salario);
-
-            Console.WriteLine("A bonificação do funcionário " + rodolfo.Nome + " será de: R$" + rodolfo.GetBonificacao());
-            Console.WriteLine("O salário total do funcionário " + rodolfo.Nome + " será de: R$" + (rodolfo.GetBonificacao() + rodolfo.Salario));
-
-            Console.WriteLine();
-            Console.WriteLine("Total de bonificações: R$" + gerenciador.GetTotalBonificacao());
-
-
+            CalcularBonificacao();
             Console.ReadLine();
+        }
+        public static void CalcularBonificacao()
+        {
+            GerenciadorBonificacao gerenciadorBonificacao = new GerenciadorBonificacao();
+
+            Designer pedro = new Designer("789.456.789-10");
+            pedro.Nome = "Pedro";
+
+            Diretor roberta = new Diretor("123.654.789-10");
+            roberta.Nome = "Roberta";
+
+            Auxiliar igor = new Auxiliar("147.258.369.78");
+            igor.Nome = "Igor";
+
+            GerenteDeConta camila = new GerenteDeConta("753.156.852-45");
+            camila.Nome = "Camila";
+
+            gerenciadorBonificacao.Registrar(pedro);
+            gerenciadorBonificacao.Registrar(roberta);
+            gerenciadorBonificacao.Registrar(igor);
+            gerenciadorBonificacao.Registrar(camila);
+
+            Console.WriteLine("Total de bonificação do mês: " + gerenciadorBonificacao.GetTotalBonificacao());
         }
     }
 }
